805909651fd806f5551dea8e326395fd0f50a4f1
oyazar27 <zarnet2@yahoo.com>
2017-05-27 19:45:16 +0300

    1
    1

--- config/webconfig.php
+++ config/webconfig.php
@@ -1,6 +1,6 @@
 <?php
 
-$host = "http://futbol-futboldata.1d35.starter-us-east-1.openshiftapps.com/";
+$host = getenv(MYSQL_HOST);
 $user = getenv(MYSQL_USER);
  $pass = getenv(MYSQL_ROOT_PASSWORD);
 $db = getenv(MYSQL_DATABASE);
