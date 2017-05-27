6101517453a891293d9351085ab42ed71741ca80
oyazar27 <zarnet2@yahoo.com>
2017-05-28 01:16:47 +0300

    service host
    host env variable

--- config/webconfig.php
+++ config/webconfig.php
@@ -1,6 +1,6 @@
 <?php
 
-echo $host = getenv(MYSQL_HOST);
+echo $host = getenv(MYSQL_SERVICE_HOST);
 echo $user = getenv(MYSQL_USER);
  $pass = getenv(MYSQL_ROOT_PASSWORD);
 echo $db = getenv(MYSQL_DATABASE);
