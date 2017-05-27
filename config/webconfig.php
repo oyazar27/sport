c4e84cead5deb1fb48c9199fb71742c08a008dc8
oyazar27 <zarnet2@yahoo.com>
2017-05-27 19:13:37 +0300

    DB
    DB

--- config/webconfig.php
+++ config/webconfig.php
@@ -1,21 +1,7 @@
-/*Obsolete
-
-
 <?php
 
-$root = 'http://project2-zarnet.rhcloud.com/'; 
+echo $user = getenv(MYSQL_USER);
+ $pass = getenv(MYSQL_ROOT_PASSWORD);
+echo $user = getenv(MYSQL_USER);
 
-$host = "$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT";
-
-$user = "adminCQjtUFm";
-
-$pass = "L6eyHsBNwsge";
-
-$db = "price_comparison";
-    
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+$db = getenv(MYSQL_DATABASE);
